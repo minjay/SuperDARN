e4fb231310e0692f85129567caba78693a7c1079
minjay <mjfan@ucar.edu>
2019-10-12 22:44:43 -0700

    Update with the correct file.

--- examine_cond_data.m
+++ examine_cond_data.m
@@ -4,15 +4,15 @@ clear
 % file examine_sam_data.
 
 % load data from different fields.
-hinfo = hdf5info('OP_2012-02-20_000000.h5');
-mag_lat = hdf5read('OP_2012-02-20_000000.h5', '/lat_grid');
-mag_lon = hdf5read('OP_2012-02-20_000000.h5', '/lon_grid');
+hinfo = hdf5info('OP_2012-02-29_000000.h5');
+mag_lat = hdf5read('OP_2012-02-29_000000.h5', '/lat_grid');
+mag_lon = hdf5read('OP_2012-02-29_000000.h5', '/lon_grid');
 mag_lat = fliplr(reshape(mag_lat, 41, 180)');
 mag_lon = fliplr(reshape(mag_lon, 41, 180)');
-ped_cond_all = hdf5read('OP_2012-02-20_000000.h5', '/Pedersen_Conductance');
+ped_cond_all = hdf5read('OP_2012-02-29_000000.h5', '/Pedersen_Conductance');
 ped_cond_all = reshape(ped_cond_all, 41, 180, size(ped_cond_all, 2));
 
-ts = hdf5read('OP_2012-02-20_000000.h5', '/ts');
+ts = hdf5read('OP_2012-02-29_000000.h5', '/ts');
 % The time series is of length 121
 % Start from 00:00:00 to 04:00:00
 % In comparison, in file examine_sam_data, the time series is of length
@@ -27,6 +27,6 @@ mag_lon = mag_lon / 180 * pi;
 mag_lon_rot = mag_lon - pi / 2;
 [x, y] = pol2cart(mag_lon_rot, mag_colat / pi * 180);
 
-ped_cond = fliplr(ped_cond_all(:, :, 1)');
+ped_cond = fliplr(ped_cond_all(:, :, 2)');
 vmag = linspace(min(ped_cond(:)), max(ped_cond(:)), 10);
 mypolar([0 2*pi], [0 max(mag_colat(:)) / pi * 180], x, y, ped_cond, vmag);
