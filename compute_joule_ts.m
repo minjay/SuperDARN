08782338d54a29debbd364215ce05d41a234856f
minjay <mjfan@ucar.edu>
2019-11-29 22:35:13 -0800

    Set tight white margin for the plot output.

--- compute_joule_ts.m
+++ compute_joule_ts.m
@@ -156,3 +156,12 @@ yyaxis right
 ae_index_ts = timeseries(ae_index, ts);
 plot(ae_index_ts, '-o')
 ylabel('AE index [nT]')
+
+ax = gca;
+outerpos = ax.OuterPosition;
+ti = ax.TightInset; 
+left = outerpos(1) + ti(1);
+bottom = outerpos(2) + ti(2);
+ax_width = outerpos(3) - ti(1) - ti(3);
+ax_height = outerpos(4) - ti(2) - ti(4);
+ax.Position = [left bottom ax_width ax_height];
