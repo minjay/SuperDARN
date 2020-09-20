8dc94361368f74cdca912e9a320b7a382ffe96db
minjay <mjfan@ucar.edu>
2020-09-20 10:29:34 -0700

    Allow some margin at the top

--- verify_kriging.m
+++ verify_kriging.m
@@ -85,7 +85,7 @@ ti = ax.TightInset;
 left = outerpos(1) + ti(1);
 bottom = outerpos(2) + ti(2);
 ax_width = outerpos(3) - ti(1) - ti(3);
-ax_height = outerpos(4) - ti(2) - ti(4);
+ax_height = outerpos(4) - ti(2) - ti(4)-0.01;
 ax.Position = [left bottom ax_width ax_height];
 
 % in-sample predictions
